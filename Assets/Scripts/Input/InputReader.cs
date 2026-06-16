3b161e410758a7ac319253b8cf33affb5389b2f4
Diavolo <2218958431@qq.com>
2026-06-16 23:30:49 +0800

    相机控制器

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -6,8 +6,10 @@ public class InputReader : MonoBehaviour
     [SerializeField] InputActionAsset inputActions;
 
     InputAction moveAction;
+    InputAction lookAction;
 
     public Vector2 MoveInput => moveAction != null ? moveAction.ReadValue<Vector2>() : Vector2.zero;
+    public Vector2 LookInput => lookAction != null ? lookAction.ReadValue<Vector2>() : Vector2.zero;
 
     void Awake()
     {
@@ -20,6 +22,7 @@ public class InputReader : MonoBehaviour
 
         InputActionMap playerMap = inputActions.FindActionMap("Player", throwIfNotFound: true);
         moveAction = playerMap.FindAction("Move", throwIfNotFound: true);
+        lookAction = playerMap.FindAction("Look", throwIfNotFound: true);
     }
 
     void OnEnable()
